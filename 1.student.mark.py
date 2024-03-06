1ebb4c9bdfe7ccfcaa4f42ef01273bfafadb8874
Killy <minhvjjp@gmail.com>
2024-03-06 11:42:58 +0700

    Add 1.student.mark.py

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,98 +1,81 @@
-# # This is a sample Python script.
-#
-# # Press Shift+F10 to execute it or replace it with your code.
-# # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-#
-#
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-#
-#
-# # Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
-#
-# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+students = []
+courses = []
 
-# Function to input the number of students in a class
-def input_number_of_students():
-    return int(input("Enter the number of students in the class: "))
-
-# Function to input student information
-def input_student_information():
+num_students = int(input("Input number of student(s): "))
+def input_students_info():
     student = {}
-    student["id"] = input("Enter student ID: ")
-    student["name"] = input("Enter student name: ")
-    student["dob"] = input("Enter student date of birth: ")
+    student['id'] = input("Input student ID: ")
+    student['name'] = input("Input student name: ")
+    student['dob'] = input("Input student date of birth: ")
     return student
+for _ in range(num_students):
+    student  = input_students_info()
+    students.append(student)
 
-# Function to input the number of courses
-def input_number_of_courses():
-    return int(input("Enter the number of courses: "))
-
-# Function to input course information
-def input_course_information():
+num_courses = int(input("Input number of course(s): "))
+def input_courses_info():
+    # global course
     course = {}
-    course["id"] = input("Enter course ID: ")
-    course["name"] = input("Enter course name: ")
+    course['id'] = input("Input course ID: ")
+    course['name'] = input("Input course name: ")
     return course
+for _ in range(num_courses):
+    course = input_courses_info()
+    courses.append(course)
 
-# Function to select a course and input marks for students in that course
-def input_student_marks(students, courses):
-    course_id = input("Enter the course ID to input marks for: ")
+# course_id = input("Input course ID to assign marks for: ")
+
+# for _ in courses:
+#     if course['id'] == course_id:
+#         for student in students:
+#             marks = float(input(f"Assign marks for student {student['name']} in course {course['name']}: "))
+#             student.setdefault("marks", {})[course_id] = marks
+#     else:
+#         print('dit con me nha no')
+
+def input_student_marks():
     for course in courses:
-        if course["id"] == course_id:
-            for student in students:
-                marks = float(input(f"Enter marks for student {student['name']} in course {course['name']}: "))
-                student.setdefault("marks", {})[course_id] = marks
+        course_id = input("Enter the course ID to input marks for: ")
+        if course_id == 'None':
             break
+        else:
+            while course['id'] != course_id:
+                course_id = input("Enter correct course ID to input marks(Or type 'None' to exit): ")
+                if course_id == 'None':
+                    break
+            if course_id == 'None':
+                break
+        for course in courses:
+            if course['id'] == course_id:
+                for student in students:
+                    marks = float(input(f"Enter marks for student {student['name']} in course {course['name']}: "))
+                    student.setdefault("marks", {})[course_id] = marks
+                break
 
-# Function to list all courses
-def list_courses(courses):
-    print("Courses:")
+def show_student_marks():
     for course in courses:
-        print(f"Course ID: {course['id']}, Course Name: {course['name']}")
-
-# Function to list all students
-def list_students(students):
-    print("Students:")
-    for student in students:
-        print(f"Student ID: {student['id']}, Student Name: {student['name']}")
-
-# Function to show student marks for a given course
-def show_student_marks(students, courses):
-    course_id = input("Enter the course ID to show student marks for: ")
-    for course in courses:
-        if course["id"] == course_id:
-            print(f"Course: {course['name']}")
-            for student in students:
-                marks = student.get("marks", {}).get(course_id, "N/A")
-                print(f"Student: {student['name']}, Marks: {marks}")
+        course_id = input("Enter the course ID to show student marks for(or type 'None' to exit): ")
+        if course_id == 'None':
             break
+        else:
+            while course['id'] != course_id:
+                course_id = input("Enter correct course ID to show student marks(Or type 'None' to exit): ")
+                if course_id == 'None':
+                    break
+            if course_id == 'None':
+                break
+        for course in courses:
+            if course["id"] == course_id:
+                print(f"Course: {course['name']}")
+                for student in students:
+                    marks = student.get("marks", {}).get(course_id, "N/A")
+                    print(f"Student: {student['name']}, Marks: {marks}")
+                break
 
-# Main function
-def main():
-    students = []
-    courses = []
+input_student_marks()
+show_student_marks()
 
-    num_students = input_number_of_students()
-    for _ in range(num_students):
-        student = input_student_information()
-        students.append(student)
+print(students)
+print(courses)
 
-    num_courses = input_number_of_courses()
-    for _ in range(num_courses):
-        course = input_course_information()
-        courses.append(course)
 
-    input_student_marks(students, courses)
-
-    list_courses(courses)
-    list_students(students)
-
-    show_student_marks(students, courses)
-
-# Run the main function
-if __name__ == "__main__":
-    main()
